d7742bfd12f9ce25b1c3f3b6bb57389dadc1d0b3
Michael Walsh <michael.c.walsh1@gmail.com>
2017-08-05 09:36:14 -0400

    deleted dummy extraneous dummy code

---
+++ js/data/realm.js
@@ -0,0 +1,20 @@
+import Realm from 'realm';
+
+class User extends Realm.Object {}
+User.schema = {
+    name: 'User',
+    primaryKey: 'id',
+    properties: {
+      id:    'int',
+      email: 'string',
+      password: 'string',
+      username: 'string',
+      name: 'string',
+      location: {type: 'string', optional: true},
+      bio: {type: 'string', optional: true},
+      avatar: {type: 'string', optional: true},
+      creationDate: 'date',
+    },
+};
+
+export default new Realm({schema: [User]});
